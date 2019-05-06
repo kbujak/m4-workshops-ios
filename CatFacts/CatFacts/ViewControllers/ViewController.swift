b39a9f376529f3c8c7f6d27b38447b59e6e03b6f
Marek Matulski <marekmatulski@gmail.com>
2019-05-06 16:11:01 +0200

    pods dependencies installed

--- CatFacts/CatFacts/ViewControllers/ViewController.swift
+++ CatFacts/CatFacts/ViewControllers/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import PureLayout
 
 class ViewController: UIViewController {
+    private let anyView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        view.addSubview(anyView)
+        anyView.backgroundColor = UIColor.blue
+        anyView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: 10,
+                                                                left: 10,
+                                                                bottom: 10,
+                                                                right: 10))
     }
 
 
---
+++ CatFacts/Pods/Target Support Files/Pods-CatFacts-CatFactsTests/Pods-CatFacts-CatFactsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CatFacts_CatFactsTests : NSObject
+@end
+@implementation PodsDummy_Pods_CatFacts_CatFactsTests
+@end
